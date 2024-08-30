2e84a65825670dd7cd3fc39a2be615965cd213b1
Titus150 <k.titus150@gmail.com>
2024-08-29 22:31:52 -0700

    Add files via upload

---
+++ GFG.java
@@ -0,0 +1,12 @@
+// Importing File Class
+import java.io.File;
+
+class GFG {
+	public static void main(String[] args)
+	{
+
+		// File name specified
+		File obj = new File("myfile.txt");
+		System.out.println("File Created!");
+	}
+}
